e0a9c302258504fd33980b1fa6f2516f0a88976b
ysgravija <tyeesheng@yahoo.com>
2023-03-03 13:04:33 +0800

    Refactoring: Decouple controller and service. Added Swagger UI

--- src/main/java/com/example/demo/model/FeatureAccessRequest.java
+++ src/main/java/com/example/demo/model/FeatureAccessRequest.java
@@ -1,13 +1,17 @@
 package com.example.demo.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
 import javax.validation.constraints.NotEmpty;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class FeatureAccessRequest {
     @NonNull
     @NotEmpty
